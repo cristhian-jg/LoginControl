510813ca1e254dbe4a4a12e3c7eab887bbe6576d
Cristhian <cristhianbay@gmail.com>
2019-12-11 17:47:25 +0100

    Creado metodo Hash para comprobar la contraseña

--- Login-Control/User-Control/LoginPanel.xaml.cs
+++ Login-Control/User-Control/LoginPanel.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,63 @@ namespace User_Control
     /// </summary>
     public partial class LoginPanel : UserControl
     {
+    
+        #region Resultados
+        public enum LoginResult
+        {
+            Ok,
+            Fail,
+            Canceled
+        }
+        #endregion
+
+        public static readonly string usuario = "Cristhian";
+        public static readonly string contrasenya = "02mK2d%s";
+
+        public static readonly System.Windows.RoutedEvent ReturnIdentifyLogin
+            = System.Windows.EventManager.RegisterRoutedEvent(
+                "Identify Login",
+                RoutingStrategy.Bubble,
+                typeof(RoutedEventHandler),
+                typeof(LoginPanel)
+                );
+
         public LoginPanel()
         {
             InitializeComponent();
+        }
+
+        static string Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        private void onClickLogin(object sender, RoutedEventArgs e)
+        {
+            bool validado = false;
+            string input;
+
+            if (tbUsuario.Text.Equals(usuario))
+            {
+                input = Hash(tbContrasenya.Text);
+
+                if (input == contrasenya)
+                {
+                    validado = true;
+                }
+                
+            }
         }
     }
 }
